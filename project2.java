11e1e76922154081d40895a5bd5881287e8fc571
DeviUthirabalan <devishree597@gmail.com>
2018-05-30 02:53:45 -0700

    loop statements

---
+++ project2.java
@@ -0,0 +1,14 @@
+package project1;
+
+
+public class project2 {
+	public static void main(String s[]) {
+	int i=0;
+	do 	
+	{
+	System.out.println("Do while i =" + i);
+	i++;
+	}
+	while(i<101);
+}
+}
