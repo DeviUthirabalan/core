1a16ac7c33bb1ffecbd1a63be640a58903e4ea3f
DeviUthirabalan <devishree597@gmail.com>
2018-05-30 03:24:52 -0700

    prize winning

---
+++ project3.java
@@ -0,0 +1,16 @@
+package project1;
+
+
+public class project3 {
+	public static void main(String s[]) {
+	int num=1111;
+	while(num>1000)
+	if(num%2==0) {
+		System.out.println("selected");
+	break;
+	}
+	if(num%2!=0)
+		System.out.println("rejected");
+	
+	}
+}
