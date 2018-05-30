6d79542e709fd6e53f4fe98e0bf07828fffee7d9
DeviUthirabalan <devishree597@gmail.com>
2018-05-30 02:29:59 -0700

    If else

---
+++ project1.java
@@ -0,0 +1,13 @@
+package project1;
+
+public class project1{
+	public static void main(String s[]) {
+	int marks=0;
+	{
+	if(marks>=50)
+		System.out.println("pass");
+	else
+		System.out.println("fail");
+	}
+	}
+}
