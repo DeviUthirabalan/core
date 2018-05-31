5aa334bd5d0f213f8a9f11e7a7879634c84371cb
DeviUthirabalan <devishree597@gmail.com>
2018-05-31 02:13:28 -0700

    ifelse

---
+++ task1.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class task1 {
+	public static void main(String s[],int Age)
+	{
+	Age=70;	
+	if(Age>=65)
+		{
+	System.out.println("Age is greater than or equal to 65");
+		}
+	else
+	System.out.println("Age is less than 65");
+	}
+	}
+
