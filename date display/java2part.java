bcead7b7ac9140e605f739c79d2180bea0949bd1
DeviUthirabalan <devishree597@gmail.com>
2018-05-31 02:14:11 -0700

    date

---
+++ date display/java2part.java
@@ -0,0 +1,12 @@
+package project2;
+
+public class java2part {
+	public static void main(String arg[]){
+	Date d1 = new Date(0,0,0);
+	d1.setDay(31);
+    d1.setMonth(5);
+    d1.setYear(2018);
+    Date d2 = new Date(1,6,2018);
+    d2.displayDate();
+}
+}
