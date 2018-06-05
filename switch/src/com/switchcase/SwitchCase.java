ef3d17f9efeb3df98a5822684215598b7974d46b
DeviUthirabalan <devishree597@gmail.com>
2018-06-04 22:38:06 -0700

    switchcase

---
+++ switch/src/com/switchcase/SwitchCase.java
@@ -0,0 +1,20 @@
+package com.switchcase;
+
+public class SwitchCase {
+	public static void main(String[] arg){
+	int Name=10;
+	switch(Name){
+	case 10:
+		System.out.println("Devi");
+		break;
+	case 20:
+		System.out.println("Dhayali");
+		break;
+	case 30:
+	    System.out.println("Dharani");
+	    break;
+   default:
+	    	 System.out.println("default");
+	}
+
+}}
