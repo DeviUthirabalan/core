de5f44d6950c00c744d7d516bcb12e2dd39d07bb
DeviUthirabalan <devishree597@gmail.com>
2018-06-04 23:28:59 -0700

    strings in switch

---
+++ stringcase/src/com/stringcase/StringCase.java
@@ -0,0 +1,23 @@
+package com.stringcase;
+
+public class StringCase {
+	public static void main(String[] arg){
+		switch("dhayali"){
+		case "dharani":
+			System.out.println("hoohoo");
+			System.out.println(" green rose");
+			break;
+		case "devi":
+			System.out.println("haahaa");
+			System.out.println("red rose");
+			break;
+		case "dhayali":
+		    System.out.println("hehe");
+		    System.out.println("pink rose");
+		    
+		    break;
+	   default:
+		    	 System.out.println("default");
+		}
+
+}}
