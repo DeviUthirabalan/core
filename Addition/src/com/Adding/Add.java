57a52a6fe95fc9d4c06498e52f46d8fd5937cf6b
DeviUthirabalan <devishree597@gmail.com>
2018-06-05 22:20:23 -0700

    Scanner class

---
+++ Addition/src/com/Adding/Add.java
@@ -0,0 +1,15 @@
+package com.Adding;
+
+import java.util.Scanner;
+
+public class Add {
+	@SuppressWarnings("resource")
+	public static void main(String agrs[]){
+		@SuppressWarnings("unused")
+		Scanner sc=new Scanner(System.in);
+		int a = 10;
+		int b = 20;
+	    int c=a+b;
+	System.out.println("c=" +c);
+		
+	}}
