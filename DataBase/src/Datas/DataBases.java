db5e077cf22ac8ef3dbf9cec866c84d9a9128bfd
DeviUthirabalan <devishree597@gmail.com>
2018-06-11 10:23:50 +0530

    arraycopy

---
+++ DataBase/src/Datas/DataBases.java
@@ -0,0 +1,14 @@
+package Datas;
+
+public  class DataBases {
+	private static String firstName= "firstName";
+	private static String lastName= "lastName";	
+	public static void main(String s[])
+ {
+		firstName="Devi";
+		lastName="Uthirabalan";
+System.out.println("firstName: "+firstName);
+System.out.println("lastName: "+lastName);
+
+
+}}
