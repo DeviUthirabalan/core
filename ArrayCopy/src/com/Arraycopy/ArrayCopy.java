46fa852a7aa914be3cb15bd5a051e5a79d903287
DeviUthirabalan <devishree597@gmail.com>
2018-06-17 15:01:36 +0530

    ArrayCopy

---
+++ ArrayCopy/src/com/Arraycopy/ArrayCopy.java
@@ -0,0 +1,14 @@
+package com.Arraycopy;
+
+public class ArrayCopy{
+
+	private static int[] arr;
+
+	public static void main(String[] args){
+		@SuppressWarnings("unused")
+		int one=arr[6];
+		int[] one1={1,2,3,4,5,6};
+		
+		System.out.println("a[3],a[],a[6]"+one1);
+	}
+}
